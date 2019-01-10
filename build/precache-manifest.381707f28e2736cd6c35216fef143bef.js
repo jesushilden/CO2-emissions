243a4dd56fdb163e1448c25ac9a12b4e66c41534
jesushilden <peter.e.hilden@gmail.com>
2019-01-10 14:21:01 +0200

    set server to use production build frontend

---
+++ build/precache-manifest.381707f28e2736cd6c35216fef143bef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef4cd042d1803f277044",
+    "url": "/static/js/main.ef4cd042.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "86820ac62300a36303962e02b5e50392",
+    "url": "/index.html"
+  }
+];
