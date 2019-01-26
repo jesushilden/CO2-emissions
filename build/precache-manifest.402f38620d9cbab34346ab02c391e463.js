fa35347191f7ae93f36e8410d1f298465b65d7f0
jesushilden <peter.e.hilden@gmail.com>
2019-01-26 14:18:59 +0200

    new production build

---
+++ build/precache-manifest.402f38620d9cbab34346ab02c391e463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e40538e049dc629f1296",
+    "url": "/static/js/main.e40538e0.chunk.js"
+  },
+  {
+    "revision": "cf61054d025a1d716aec",
+    "url": "/static/js/1.cf61054d.chunk.js"
+  },
+  {
+    "revision": "e40538e049dc629f1296",
+    "url": "/static/css/main.6fbd94a7.chunk.css"
+  },
+  {
+    "revision": "c3d1f16bb8dbbd890a3d38edd95455b0",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e01b88e7ef9b1c205d9aefe607ecbe67.js"
+  "/precache-manifest.402f38620d9cbab34346ab02c391e463.js"
 );
 
 workbox.clientsClaim();
